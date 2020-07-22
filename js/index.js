538cb51e72b8060ff78e539176d4f44e3f35e62b
gyuri <fnffkdll@naver.com>
2020-07-23 05:20:49 +0900

    update - responsible

--- js/index.js
+++ js/index.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
+    //
+    AOS.init();
+    
+    //header_nav
+    $('.store a').hover(function(){
+        $('.nav_store').stop().animate({height:'350px'},300);
+    },function(){
+        $('.nav_store').stop().animate({height:'0'},300);
+    })
     
     //main_best - slide
     var swiper = new Swiper('.main_best .swiper-container', {
@@ -12,5 +21,19 @@ $(document).ready(function(){
         el: '.main_best .swiper-pagination',
         clickable: true,
       },
+      breakpoints: {
+//        640: {
+//          slidesPerView: 2,
+//          spaceBetween: 20,
+//        },
+        480: {
+          slidesPerView: 2,
+          slidesPerGroup: 2,
+        },
+        768: {
+          slidesPerView: 3,
+          slidesPerGroup: 3,
+        },
+      }
     });
 })
